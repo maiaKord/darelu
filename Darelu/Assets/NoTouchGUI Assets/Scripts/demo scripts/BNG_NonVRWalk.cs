06784faeb67a3e5aca52f01e9a3d2ff01a6076ca
Conflei <Felix1645018>
2016-04-16 00:58:26 -0430

    Plugins: iTween, No Touch for VR & Particles

---
+++ Darelu/Assets/NoTouchGUI Assets/Scripts/demo scripts/BNG_NonVRWalk.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BNG_NonVRWalk : MonoBehaviour
+{
+
+    //private static FPSInputControllerForNoTouchGUI fpsRef;
+
+	// Use this for initialization
+	void Start () {
+
+        //fpsRef = transform.GetComponent<FPSInputControllerForNoTouchGUI>();
+	}
+
+    void ActivateAutoWalk()
+    {
+        //fpsRef.autowalk = 1;
+    }
+
+    void DeactivateAutoWalk()
+    {
+        //fpsRef.autowalk = 0;
+    }
+}
